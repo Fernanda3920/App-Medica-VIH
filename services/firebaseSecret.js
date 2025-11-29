7ce73a99ac51181b9730ca0eaffff3374957ee9d
Fernanda Nevárez <167030876+Fernanda3920@users.noreply.github.com>
2025-11-28 17:39:09 -0800

    Agregar estructura de firebase secret
    Replaced sensitive Firebase configuration values with placeholders.

---
+++ services/firebaseSecret.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "XXXXXXX",
+  authDomain: "XXXXXXX",
+  projectId: "XXXXXXX",
+  storageBucket: "XXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXX",
+  appId: "XXXXXXXXX",
+  measurementId: "XXXXXXXXX"
+};
